e7bbfb9242058ac9f6b3d82054667d3953027f77
Daniel Munoz <daniel.munozdev@gmail.com>
2017-12-04 15:09:50 -0800

    Add a function to create an li and append it to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@
 let iceCreams = ['Mint chocolate chip', 'Vanilla', 'Strawberry'];
 let imgSources = ['img/mint-chocolate-chip.png', 'img/vanilla.png', 'img/strawberry.png'];
 let imgAlts = ['Picture of a mint chocolate chip ice cream cone', 'Picture of a vanilla ice cream cone', 'Picture of a strawberry ice cream cone'];
+
+// User interface (or front-end) logic:
+// Function to create an li and append to main ul
+function createLi(iceCreamFlavor, imageSourceName, imageAltName) {
+  let li = $('<li></li>');
+  let p = $('<p><span>' + iceCreamFlavor + '<span></p>');
+  let img = $('<img src=\'' + imageSourceName + '\' alt=\'' + imageAltName + '\'>');
+  li.append(p);
+  li.append(img);
+  $('main ul').append(li);
+}
