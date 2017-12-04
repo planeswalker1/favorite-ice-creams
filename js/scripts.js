dfd6d31aa5ffc32985e7b099efc7448c3c49fc3f
Daniel Munoz <daniel.munozdev@gmail.com>
2017-12-03 18:03:17 -0800

    Add arrays with content to append to the DOM

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Business (or back-end) logic:
+// arrays of ice cream flavors image sources and image alt text
+let iceCreams = ['Mint chocolate chip', 'Vanilla', 'Strawberry'];
+let imgSources = ['img/mint-chocolate-chip.png', 'img/vanilla.png', 'img/strawberry.png'];
+let imgAlts = ['Picture of a mint chocolate chip ice cream cone', 'Picture of a vanilla ice cream cone', 'Picture of a strawberry ice cream cone'];
